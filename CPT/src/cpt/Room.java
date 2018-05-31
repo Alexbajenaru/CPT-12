4bafca64a87a024f0dd312c7d6ffce41dd6b657e
Daniel Bassani <Daniel Bassani@10.112.1.235>
2018-05-31 13:17:10 -0400

    :)

---
+++ CPT/src/cpt/Room.java
@@ -0,0 +1,86 @@
+package cpt;
+
+import javafx.scene.shape.Shape;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+
+public class Room {
+
+    private Group root = new Group();
+    private Scene scene = new Scene(root, 500, 500);
+    private Character character = new Character(200, 200, true, 30, 40);
+
+    public Room() {
+        this.root = root;
+        this.scene = scene;
+        this.character = character;
+    }
+
+    public Character getCharacter() {
+        return character;
+    }
+
+    public Group getRoot() {
+        return root;
+    }
+
+    public Scene getScene() {
+        return scene;
+    }
+
+    public void setCharacter(Character character) {
+        this.character = character;
+    }
+
+    public void setRoot(Group root) {
+        this.root = root;
+    }
+
+    public void setScene(Scene scene) {
+        this.scene = scene;
+    }
+
+    public void update() {
+        scene.setOnKeyPressed(e -> {
+            if (e.getCode() == KeyCode.LEFT) {
+                character.setTranslateX(character.getTranslateX() - 10);
+
+                for (int i = 0; i < root.getChildren().size() - 1; i++) {
+                    Shape shape = (Shape) root.getChildren().get(i);
+                    if (character.getBoundsInParent().intersects(shape.getBoundsInParent())) {
+                        character.setTranslateX(character.getTranslateX() + 10);
+                    }
+                }
+
+            } else if (e.getCode() == KeyCode.RIGHT) {
+                character.setTranslateX(character.getTranslateX() + 10);
+
+                for (int i = 0; i < root.getChildren().size() - 1; i++) {
+                    Shape shape = (Shape) root.getChildren().get(i);
+                    if (character.getBoundsInParent().intersects(shape.getBoundsInParent())) {
+                        character.setTranslateX(character.getTranslateX() - 10);
+                    }
+                }
+            } else if (e.getCode() == KeyCode.DOWN) {
+                character.setTranslateY(character.getTranslateY() + 10);
+
+                for (int i = 0; i < root.getChildren().size() - 1; i++) {
+                    Shape shape = (Shape) root.getChildren().get(i);
+                    if (character.getBoundsInParent().intersects(shape.getBoundsInParent())) {
+                        character.setTranslateY(character.getTranslateY() - 10);
+                    }
+                }
+            } else if (e.getCode() == KeyCode.UP) {
+                character.setTranslateY(character.getTranslateY() - 10);
+
+                for (int i = 0; i < root.getChildren().size() - 1; i++) {
+                    Shape shape = (Shape) root.getChildren().get(i);
+                    if (character.getBoundsInParent().intersects(shape.getBoundsInParent())) {
+                        character.setTranslateY(character.getTranslateY() + 10);
+                    }
+                }
+            }
+        });
+    }
+}
